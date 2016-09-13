d5cad0be4eef0ee0aed16c22b80556c599b60942
Omar Laurino <olaurino@cfa.harvard.edu>
2016-09-13 01:53:14 -0400

    fix latex files

---
+++ bin/pdf.bat
@@ -0,0 +1,9 @@
+
+if not exist output\NUL mkdir output
+
+pandoc -s -N --toc --template %~dp0..\ivoa-template.tex^
+  --latex-engine=xelatex --listings^
+  --filter %~dp0../filters/include.py --filter pandoc-crossref --filter pandoc-citeproc^
+  --css %~dp0..\style\ivoa_doc.css --css %~dp0..\style\ivoa-plus.css^
+  %*^
+  -o output\full.pdf
---
+++ bin/pdf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -xe
+
+bindir=$( cd -P "$( dirname "$0" )" && pwd )
+if [ -h "$0" ]; then
+  bindir=$( cd -P "$( dirname "`readlink -n "$0"`" )" && pwd )
+fi
+
+mkdir -p output
+
+pandoc -s -N --toc --template $bindir/../ivoa-template.tex\
+  --latex-engine=xelatex --listings\
+  --filter $bindir/../filters/include.py --filter pandoc-crossref --filter pandoc-citeproc\
+  --css $bindir/../style/ivoa_doc.css --css $bindir/../style/ivoa-plus.css\
+  $*\
+  -o output/full.html
